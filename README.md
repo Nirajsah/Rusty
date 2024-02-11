f9c69b18edf4d038e7556836094945b013301358
Nirajsah <nirajsahy4aj@gmail.com>
2024-02-11 20:06:30 +0530

    node

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Rusty
 
 <# **_A Repo for learing every rust topic_**
-
--- Topics will be added with every new commit --
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct Node {
+    data: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(data: i32) -> Self {
+        Node { data, next: None }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let one = Node::new(32);
+    let two = Node {
+        data: 12,
+        next: Some(Box::new(one)),
+    };
+    println!("{:?}", two.next.unwrap().data);
 }
